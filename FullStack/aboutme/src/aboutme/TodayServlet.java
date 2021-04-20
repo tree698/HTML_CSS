6422511c31ae2ba3a1d2163eeee2b3f2ce51a9e8
Chanwoo <tree698@gmail.com>
2021-04-20 15:38:08 +0900

    LocalDateTime servlet

---
+++ FullStack/aboutme/src/aboutme/TodayServlet.java
@@ -0,0 +1,48 @@
+package aboutme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<head><title>몇시에요</title></head>");
+		out.print("<body>");
+		
+		LocalDateTime current = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:ss");
+		String formatted = current.format(formatter);
+		out.println("현재시간: " + formatted);
+		
+		out.println("</body>");
+		out.println("<html>");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
