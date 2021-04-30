0ab4bebd1fcf3327f13b0a8309b41f20b18eb5a2
Chanwoo <tree698@gmail.com>
2021-04-30 18:16:10 +0900

    .

--- FullStack/firstweb/src/example/ApplicationScope02.java
+++ FullStack/firstweb/src/example/ApplicationScope02.java
@@ -1,33 +1,39 @@
 package example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ApplicationScope02
- */
+
 @WebServlet("/ApplicationScope02")
 public class ApplicationScope02 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public ApplicationScope02() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/HTML, utf-8");
+		PrintWriter out = response.getWriter();
+		
+		ServletContext application = getServletContext();
+		
+		try {
+		int value = (int) application.getAttribute("value");
+		value ++;
+		
+		out.println("<h1>value: " + value + "</h1>");
+		}catch(NullPointerException ex) {
+			out.println("value 값이 설정되지 않았습니다.");
+		}
 	}
-
 }
