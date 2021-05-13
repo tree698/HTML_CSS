0110828c8777d01c2d0e5eeb2e22d56ac1c17873
Chanwoo <tree698@gmail.com>
2021-05-13 23:32:01 +0900

    .

---
+++ 4_MySQL/user.sql
@@ -0,0 +1,13 @@
+create database sampDB;
+DROP DATABASE sampDB;
+
+USE shopDB;
+SELECT * FROM memberTBL;
+DELETE FROM memberTBL;
+
+USE shopDB;
+SELECT * FROM membertbl;
+DELETE FROM membertbl WHERE memberID = 'Jae';
+
+USE employees;
+SELECT * FROM 	employees;
