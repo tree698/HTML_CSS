6908ccb82b55e6c0f1a6f50c4604cdf5342f16ee
Chanwoo <tree698@gmail.com>
2021-05-21 16:01:22 +0900

    .

---
+++ 2_JavaScript/DreamCoding/main.js
@@ -0,0 +1,15 @@
+
+// ECMA Sctipt 5에 포함, 비상식적 코드 사용 금지 
+'use strict';
+
+
+// log 함수를 이용해 콘솔창에 출력
+console.log('Hello World!');
+
+
+// 정의 되지 않은 변수이므로 콘솔창에서 에러 발생 
+// a = 6;
+
+let a;
+a = 6;
+console.log(a);
