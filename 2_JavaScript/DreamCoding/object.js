1507c3e8314e8efa53762f520802d63d85d61cf3
Chanwoo <tree698@gmail.com>
2021-05-29 22:30:19 +0900

    .

--- 2_JavaScript/DreamCoding/object.js
+++ 2_JavaScript/DreamCoding/object.js
@@ -5,6 +5,7 @@
 // object = { key : value};
 
 
+// console.clear();
 
 // 1. Literals and properties
 // Object 선언: object literal syntax
@@ -69,4 +70,68 @@ function Person(name, age) {
 }
 
 
-// 5. in operator: 
+// 5. in operator: property existence check (key in obj)
+console.log('name' in ellie);
+console.log('age' in ellie);
+console.log('random' in ellie);
+console.log(ellie.random);
+
+
+
+// 6. for..in vs. for..of
+// for (key in obj)  object에 있는 key에 대해 순차적으로 순회한다 
+for (key in ellie) {
+    console.log(key);
+}
+
+// for (value of iterable) 배열과 같이 iterable한 각 원소에 대해 순차적으로 순회한다
+const array = [1,2,3,4,5];
+// for (let i=0 ; i < array.length ; i++) {
+//     console.log(array[i]);
+// }
+for (value of array) {
+    console.log(value);
+}
+
+
+
+// 7. Fun cloning
+// object.assign(dest, [obj1, obj2, obj3...])
+const user = { name: 'ellie', age: '20'};
+// 같은 reference를 가리킨다. 이것은 복사하는 것이 아니다 
+const user2 = user;
+// 그래서 user2의 이름을 변경하니 user도 함께 변경된다
+user2.name = 'coder';
+console.log(user);
+
+// object 복사 방법(복사후 별도의 객체가 됨) : old way
+const user3 = {}
+for (key in user) {
+    user3[key] = user[key];
+}
+console.log(user3);
+// user3을 변경하여도 user는 변경되지 않는다. 
+user3.name = 'Lee';
+console.log(user3);
+console.log(user);
+
+// New way
+const user4 = {};
+Object.assign(user4, user);
+console.log(user4);
+// user4를 변경하여도 user는 변경되지 않는다. 
+user4.name = 'Chan';
+console.log(user4);
+console.log(user);
+
+// 상기 코드를 줄이면...
+const user5 = Object.assign({}, user);
+console.log(user5);
+
+// another example
+const fruit1 = { color: 'red'};
+const fruit2 = { color:'blue', size: 'big'};
+const mixed = Object.assign({}, fruit2, fruit1);
+// 나중 것(fruit1)이 덮어 쓰면서 출력된다 
+console.log(mixed.color);
+console.log(mixed.size); 
