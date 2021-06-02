85d581d4afa0bb9966921f7cd02a4d9b4130497c
Chanwoo <tree698@gmail.com>
2021-06-02 20:30:18 +0900

    Object to JSON

--- 2_JavaScript/DreamCoding/json.js
+++ 2_JavaScript/DreamCoding/json.js
@@ -8,9 +8,37 @@
 // object <===> string
 
 
-// 1. Object to JSON
+// 1. Object to JSON  =>  stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${name} can jump`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+// replacer로 원하는 목록만 추가 
+json = JSON.stringify(rabbit, ['name', 'color']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
 
 
 
-// 2. JSON to Object 
+// 2. JSON to Object  =>  parse(json) 
 
