bf7ffbae5b9fb0e3b7285642adacd42417c63a37
Chanwoo <tree698@gmail.com>
2021-06-02 20:13:18 +0900

    JSON

---
+++ 2_JavaScript/DreamCoding/json.js
@@ -0,0 +1,16 @@
+// JSON (JavaScript Object Notation)
+// simplest data interchange format
+// lightweight text-based structure
+// easy to Read 
+// key-value pair
+// used for serialization and transmission of data between the network connections
+// independent programming language and platform
+// object <===> string
+
+
+// 1. Object to JSON
+
+
+
+// 2. JSON to Object 
+
