29122c3b775308f0c04d8fba77d0b6c05e81f3b8
Chanwoo <tree698@gmail.com>
2021-06-02 20:47:50 +0900

    JSON to Object

--- 2_JavaScript/DreamCoding/json.js
+++ 2_JavaScript/DreamCoding/json.js
@@ -41,4 +41,28 @@ console.log(json);
 
 
 // 2. JSON to Object  =>  parse(json) 
+const obj = JSON.parse(json);
+console.log(obj);
+
+// 당초 만들었던 jump 함수는 json으로 만들때 포함되지 않아 obj.jump() / birthDate은 에러
+rabbit.jump()
+// obj.jump()
+console.log(rabbit.birthDate.getDate());
+// console.log(obj.birthDate.getDate());
+
+
+// revive로 json으로 만들때 포함되지 않은 함수등을 다시 포함 
+const obj2 = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+
+// 잘 실행됨
+console.log(obj2.birthDate.getDate());
+// jump 함수도 실행되기 위해서는 55줄의 코드에 추가해야 한다 
+// obj2.jump();
+
+
+
+
 
